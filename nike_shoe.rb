5cc108cf0c75a5e8da857fcdd473ab05062323d6
Natalia0727 <oicafn@gmail.com>
2018-06-07 22:34:50 +0000

    automatically backed up by learn

--- nike_shoe.rb
+++ nike_shoe.rb
@@ -1,7 +1,13 @@
 class Shoes
   #need atributes for color, laces, base and swoosh
-  def color=(color_base) #gets the color for the base of a shoe 
-    @this_shoes_color =color_base
+  def color=(color_base) #gets color for the base of shoes
+    @this_shoes_color =color_base #creates variable for THIS INSTANCE
   end
   
+  def color #tell user color of shoe
+    @this_shoes_color
+  end 
+
 end 
+sams_shoe = Shoes.new
+sams_shoe.color="red"
